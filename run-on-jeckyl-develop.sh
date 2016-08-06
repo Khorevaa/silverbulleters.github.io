f48286fa67c39d26e7d71c14185baf06d155bccc
Alexey Lustin <alustin@silverbulleters.org>
2016-08-06 11:37:24 +0300

    добавлен локальный docker контейнер #210

--- run-on-jeckyl-develop.sh
+++ run-on-jeckyl-develop.sh
@@ -1,5 +1,9 @@
 echo "Я думаю что docker у вас установлен нативно"
 echo "И даже если у вас Windows - интерпретатор bash у вас доступен в PATH"
 
-docker run --rm --label=jekyll --name=jeckyl-develop --volume=$(pwd):/srv/jekyll \
+if [ -z $PWD ]; then
+    PWD=$(pwd)
+fi  
+
+docker run --rm --label=jekyll --name=jeckyl-develop --volume=$PWD:/srv/jekyll \
     -it -p 127.0.0.1:4000:4000 jekyll/jekyll || docker stop jeckyl-develop
